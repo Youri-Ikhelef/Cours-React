d25847c352c0239a1166af8c034e6efe0a690850
Youri-Ikhelef <ikhelef.y@gmail.com>
2024-01-23 23:17:50 +0100

    Modifications visant à diviser le code et centralisé la logique à l'aide du contexte API avec son objet Context comprenant ses composants Provider et Consumer et un composant d'ordre supérieur withContext ainsi qu'un hook personalisé useAppContext. Finalisation du projet en refactorisant et divisant la base de code en plusieurs composants.

---
+++ src/components/Select.js
@@ -0,0 +1,26 @@
+import { useAppContext } from "../context";
+
+function Select() {
+  const { dispatch } = useAppContext();
+
+  const options = ["All", "Completed"];
+  const handleOnSelect = (e) => {
+    dispatch({ type: "select", payload: { option: e.target.value } });
+  };
+  return (
+    <div className="d-flex justify-content-end align-items-center my-3 ">
+      <select
+        className="select form-select form-control form-control-sm"
+        onChange={handleOnSelect}
+      >
+        {options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default Select;
